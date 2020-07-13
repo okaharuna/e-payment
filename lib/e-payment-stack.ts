dae6d2ab281dc3099e3a597b28707a3b03235e42
CM-haruna-oka <oka.haruna@classmethod.jp>
2020-07-13 21:20:26 +0900

    feat: stefunction追加

--- lib/e-payment-stack.ts
+++ lib/e-payment-stack.ts
@@ -1,9 +1,61 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as sfn from '@aws-cdk/aws-stepfunctions';
+import * as tasks from '@aws-cdk/aws-stepfunctions-tasks';
+import { Secret } from '@aws-cdk/aws-secretsmanager';
+import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
+import * as iam from '@aws-cdk/aws-iam';
+const USER_TABLE_NAME = 'Users';
+const PAYMENT_TABLE_NAME = 'Payments';
 
 export class EPaymentStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Lambda
+    // const CheckUserData = new lambda.Function(this, 'CheckUserDataFn', {
+    //   functionName: 'CheckUserData',
+    //   runtime: lambda.Runtime.NODEJS_12_X,
+    //   code: lambda.Code.fromAsset('dist/lambda/check_user_data'),
+    //   handler: 'index.handler'
+    // });
+
+    const CheckUserData = new NodejsFunction(this, 'CheckUserDataFn', {
+      functionName: 'CheckUserData',
+      runtime: lambda.Runtime.NODEJS_12_X,
+      entry: 'lambda/check_user_data/index.ts',
+      handler: "handler"
+    });
+
+
+    const StripeCharge = new NodejsFunction(this, 'StripeChargeFn', {
+      functionName: 'StripeCharge',
+      runtime: lambda.Runtime.NODEJS_12_X,
+      entry: 'lambda/stripe_charge/index.ts',
+      handler: 'handler'
+    });
+
+    const Notify = new NodejsFunction(this, 'NotifyFn', {
+      functionName: 'Notify',
+      runtime: lambda.Runtime.NODEJS_12_X,
+      entry: 'lambda/notify/index.ts',
+      handler: 'handler'
+    });
+
+    // Step function
+    const task = new tasks.LambdaInvoke(this, 'CheckUserData', {
+      lambdaFunction: CheckUserData
+    });
+    const choice = new sfn.Choice(this, 'ChoicePaymentMethod');
+    choice.when(sfn.Condition.stringEquals('$.payment_method', 'stripe'), new tasks.LambdaInvoke(this, 'StripeCharge', {
+      lambdaFunction: StripeCharge
+    }));
+    choice.otherwise(new tasks.LambdaInvoke(this, 'Notify', {
+      lambdaFunction: Notify
+    }));
+
+    const definition = task.next(choice);
+    new sfn.StateMachine(this, 'Payment', { definition });
+
   }
 }
--- test/e-payment.test.ts
+++ test/e-payment.test.ts
@@ -1,13 +1,12 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import { expect as expectCDK, matchTemplate, MatchStyle, haveResource } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as EPayment from '../lib/e-payment-stack';
 
-test('Empty Stack', () => {
+test('EPayment Stack', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new EPayment.EPaymentStack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expectCDK(stack).to(haveResource("AWS::StepFunctions::StateMachine", {
+    }))
 });

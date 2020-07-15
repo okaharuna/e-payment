b10f1ccc7245f415582657058325d27e375cfa9c
CM-haruna-oka <oka.haruna@classmethod.jp>
2020-07-15 16:15:10 +0900

    UsersテーブルとPaymentsテーブルを追加

--- lib/infra-init-stack.ts
+++ lib/infra-init-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core'
 import { Secret } from '@aws-cdk/aws-secretsmanager'
 import * as iam from '@aws-cdk/aws-iam'
+import * as dynamodb from '@aws-cdk/aws-dynamodb'
 
 export class InfraInitStack extends cdk.Stack {
     static lambdaCommonRole: iam.Role
@@ -8,8 +9,7 @@ export class InfraInitStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props)
 
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const StripeSecret = new Secret(this, id, {
+        new Secret(this, id, {
             secretName: 'Stripe',
         })
 
@@ -31,9 +31,37 @@ export class InfraInitStack extends cdk.Stack {
                     'logs:CreateLogStream',
                     'logs:PutLogEvents',
                     'secretsmanager:GetSecretValue',
+                    'dynamodb:PutItem',
+                    'dynamodb:DeleteItem',
+                    'dynamodb:GetItem',
+                    'dynamodb:Scan',
+                    'dynamodb:Query',
+                    'dynamodb:UpdateItem',
                 ],
                 resources: ['*'],
             })
         )
+
+        new dynamodb.Table(this, 'UserTable', {
+            tableName: 'Users',
+            partitionKey: {
+                name: 'userId',
+                type: dynamodb.AttributeType.STRING,
+            },
+            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+        })
+
+        new dynamodb.Table(this, 'PaymentTable', {
+            tableName: 'Payments',
+            partitionKey: {
+                name: 'storeCode',
+                type: dynamodb.AttributeType.STRING,
+            },
+            sortKey: {
+                name: 'paymentId',
+                type: dynamodb.AttributeType.STRING,
+            },
+            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+        })
     }
 }

c531fad1bc1c579a00a5c43129ece024ff428f88
CM-haruna-oka <oka.haruna@classmethod.jp>
2020-07-20 23:46:27 +0900

    feat: 支払い方法をチェック

--- src/functions/check_user_data/index.ts
+++ src/functions/check_user_data/index.ts
@@ -1,26 +1,37 @@
 import { Context } from 'aws-lambda'
-// import * as AWS from 'aws-sdk';
-// const dynamoDB = new AWS.DynamoDB.DocumentClient();
+import { Users } from '../../shared/Users'
+import { User, PaymentMethod } from '../../shared/types'
+const users = new Users()
 
-interface Payment {
-    user_id: string
+interface Params {
+    userId: string
     amount: number
 }
 
 interface Response {
-    user_id: string
+    userId: string
     amount: number
-    payment_method: 'stripe' | 'other'
+    paymentMethod: PaymentMethod
+}
+
+const check = async (user: User): Promise<any> => {
+    if (user.stripePaymentMethod)
 }
 
 export const handler = async (
-    event: Payment,
+    event: Params,
     context: Context
 ): Promise<Response> => {
     console.info({ event })
     console.info({ context })
-    // TODO
-    const result: any = event
-    result.payment_method = 'stripe'
+    const user = await users.get(event.userId)
+    if (!user) throw new Error('Unregistered user.')
+
+    const paymentMethod: PaymentMethod = user.stripePaymentMethod ? 'stripe' : 'post-pay'
+    const result: Response = {
+        amount: event.amount,
+        userId: event.userId,
+        paymentMethod
+    }
     return result
 }
